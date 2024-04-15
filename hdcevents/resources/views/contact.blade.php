ea886a4d2a190fc6034d0c617fcd0b9e34c1e19e
MiguelLSantos <151861511+MiguelLSantos@users.noreply.github.com>
2024-04-15 16:59:32 -0300

    Um moi de coisa
    * Fui ate a aula 08

---
+++ hdcevents/resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.main')
+ @section('title', 'Contatos')
+ @section('content')
+
+ <h1>Ola</h1> 
+
+@endsection
---
+++ hdcevents/resources/views/products.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.main')
+ @section('title', 'Produtos')
+ @section('content')
+
+ <h1>Mundo</h1> 
+
+@endsection
