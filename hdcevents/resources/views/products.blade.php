da1f57e4d5fd8eee29c699e1fcff7bcca51a89c7
MiguelLSantos <151861511+MiguelLSantos@users.noreply.github.com>
2024-04-16 16:59:57 -0300

    sjhsjhs

--- hdcevents/resources/views/products.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.main')
- @section('title', 'Produtos')
- @section('content')
-
- <h1>Mundo</h1> 
-
-@endsection
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -1,15 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/contatos', function () {
-    return view('contact');
-});
-
-Route::get('/produtos', function () {
-    return view('products');
-});
+Route::get('/',[EventController::class, 'index']);
+Route::get('/events/create',[EventController::class, 'create']);
+Route::post('/events',[EventController::class, 'store']);
