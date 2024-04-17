450d04eccd383e016320e6c68a582dfb1daa01b2
MiguelLSantos <151861511+MiguelLSantos@users.noreply.github.com>
2024-04-17 16:46:34 -0300

    asioc ed iom mU
    * Aula 21

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -4,25 +4,37 @@
 
      <div id="search-container" class="col-md-12">
          <h1>Busque um evento</h1>
-         <form action="">
+         <form action="/" method="GET">
              <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
          </form>
      </div>
      <div id="events-container" class="col-md-12">
-         <h2>Próximos Eventos</h2>
-         <p class="subtitle">Veja os eventos dos próximos dias</p>
+        @if ($search)
+            <h2>Buscar por: {{$search}}</h2>
+        @else
+            <h2>Próximos Eventos</h2>
+        @endif
+        @if (!$search)
+            <p class="subtitle">Veja os eventos dos próximos dias</p>
+        @endif
          <div id="cards-container" class="row">
              @foreach ($events as $event)
                  <div class="card col-md-3">
-                     <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+                     <img src="/img/{{ empty($event->image) ? 'event_placeholder.jpg' : "events/$event->image" }}"
+                         alt="{{ $event->title }}">
                      <div class="card-body">
-                         <p class="card-date">16/04/2024</p>
+                         <p class="card-date">{{ date('d/m/y', strtotime($event->date)) }}</p>
                          <h5 class="card-title">{{ $event->title }}</h5>
                          <p class="card-participants">N° Participantes</p>
-                         <a href="#" class="btn btn-primary">Saber mais</a>
+                         <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
                      </div>
                  </div>
              @endforeach
+             @if (count($events) == 0 && $search)
+                <p>Não foi possivel encontar nenhum evento com {{$search}} <a href="/">Ver todos!</a> </p>
+             @elseif(count($events) == 0)
+                <p>Não há eventos disponível!</p>
+             @endif
          </div>
      </div>
 
